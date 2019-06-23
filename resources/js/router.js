5e706380e3a08c37b724da5f682ee99b7f744ccb
shonansurvivors <shonansurvivors@gmail.com>
2019-06-23 14:19:19 +0900

    vue routing

---
+++ resources/js/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import PhotoList from './pages/Photolist.vue'
+import Login from './pages/Login.vue'
+
+/* RouterViewを使うため */
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+/* app.jsでインポートするため */
+export default router
