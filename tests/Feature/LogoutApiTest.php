9cbdaacd34884d7656826ca7e4d416c4b1f0ecb0
shonansurvivors <shonansurvivors@gmail.com>
2019-06-23 16:26:04 +0900

    ユーザーログアウトテスト

---
+++ tests/Feature/LogoutApiTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LogoutApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    public function test_認証済みのユーザーをログアウトさせる()
+    {
+        $response = $this->actingAs($this->user)
+            ->json('POST', route('logout'));
+
+        $response->assertStatus(200);
+        $this->assertGuest();
+    }
+}
