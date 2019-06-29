30ce2507ca7b7c2bdd87c3cc60cface7d060cae7
shonansurvivors <shonansurvivors@gmail.com>
2019-06-30 00:52:05 +0900

    Login.vueのloginメソッドでauthストアのアクションを呼ぶ

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -59,8 +59,9 @@
       }
     },
     methods: {
-      login () {
-        console.log(this.loginForm)
+      async login () {
+        await this.$store.dispatch('auth/login', this.loginForm)
+        this.$router.push('/')
       },
       async register () {
         // アクションを呼び出すには dispatch メソッドを使う
