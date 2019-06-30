50605e223be79ab083e0114372bd8cd476e6211a
shonansurvivors <shonansurvivors@gmail.com>
2019-06-30 12:03:17 +0900

    Login/RegisterとLogoutリンクの出し分け

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
-    <button class="button button--link" @click="logout">Logout</button>
-    <RouterLink class="button button--link" to="/login">
+    <button class="button button--link" v-if="isLogin" @click="logout">Logout</button>
+    <RouterLink v-else class="button button--link" to="/login">
       Login / Register
     </RouterLink>
   </footer>
@@ -12,6 +12,11 @@
       async logout () {
         await this.$store.dispatch('auth/logout')
         this.$router.push('/login')
+      }
+    },
+    computed: {
+      isLogin () {
+        return this.$store.getters['auth/check']
       }
     }
   }
